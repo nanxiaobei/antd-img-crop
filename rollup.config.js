7631d89a39c4902b29d962d2c05c23cb82fcbf0d
nanxiaobei <nanxiaobei@gmail.com>
2018-11-28 20:05:44 +0800

    Update to fit `@babel/runtime`

--- rollup.config.js
+++ rollup.config.js
@@ -1,19 +1,22 @@
-import sass from 'rollup-plugin-sass';
-import babel from 'rollup-plugin-babel';
 import { eslint } from 'rollup-plugin-eslint';
+import babel from 'rollup-plugin-babel';
+import sass from 'rollup-plugin-sass';
 
 import pkg from './package.json';
 
 const env = process.env.NODE_ENV;
+const dependencies = [
+  ...Object.keys(pkg.peerDependencies),
+  ...Object.keys(pkg.dependencies),
+];
+
 const config = {
   input: 'src/index.jsx',
   output: { format: env, indent: false },
-  external: [
-    ...Object.keys(pkg.peerDependencies || {}),
-    ...Object.keys(pkg.dependencies || {}),
-    'antd/es/modal/style/css',
-    'antd/es/modal',
-  ],
+  external: (id) => {
+    if (dependencies.includes(id)) return true;
+    if (id.includes('@babel/runtime/') || id.includes('antd/')) return true;
+  },
   plugins: [
     eslint({
       throwOnError: true,
@@ -21,12 +24,12 @@ const config = {
       include: ['src/**/*.(js|jsx)'],
       exclude: ['node_modules/**'],
     }),
-    sass({
-      insert: true,
-    }),
     babel({
       exclude: 'node_modules/**',
       runtimeHelpers: true,
+    }),
+    sass({
+      insert: true,
     }),
   ],
 };
