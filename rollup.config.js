4ca8e4654c41c0c55f72937c07354cedc06f6404
nanxiaobei <nanxiaobei@gmail.com>
2018-11-28 16:02:35 +0800

    Add build environment

---
+++ rollup.config.js
@@ -0,0 +1,33 @@
+import sass from 'rollup-plugin-sass';
+import babel from 'rollup-plugin-babel';
+import { eslint } from 'rollup-plugin-eslint';
+
+import pkg from './package.json';
+
+const env = process.env.NODE_ENV;
+const config = {
+  input: 'src/index.js',
+  output: { format: env, indent: false },
+  external: [
+    ...Object.keys(pkg.peerDependencies || {}),
+    ...Object.keys(pkg.dependencies || {}),
+    'antd/es/modal/style/css',
+    'antd/es/modal',
+  ],
+  plugins: [
+    eslint({
+      throwOnError: true,
+      throwOnWarning: true,
+      include: ['src/**/*.(js|jsx)'],
+      exclude: ['node_modules/**'],
+    }),
+    sass({
+      insert: true,
+    }),
+    babel({
+      exclude: 'node_modules/**',
+    }),
+  ],
+};
+
+export default config;
