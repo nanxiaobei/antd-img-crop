337dfa678b72b8a1c048d8425487f75ec6c440af
nanxiaobei <nanxiaobei@gmail.com>
2019-01-14 03:16:59 +0800

    Refine code

--- src/index.jsx
+++ src/index.jsx
@@ -4,42 +4,40 @@ import ReactCrop, { getPixelCrop } from 'react-image-crop';
 import { Modal } from 'antd';
 import './index.scss';
 
+const defaultState = {
+  // Modal
+  modalVisible: false,
+  modalWidth: 520,
+  // ReactCrop
+  src: null,
+  crop: {},
+  pixelCrop: {},
+};
+// 获取 crop 的值
+const getCropValues = (naturalWidth, naturalHeight, ratio, aspect) => {
+  // 注意，此处 width, height, x, y 均为百分比的值，如 "width: 80"，即为占比 "80%"
+  // @link: https://github.com/DominicTobias/react-image-crop#crop-required
+  const width = ((naturalHeight * ratio * aspect) / naturalWidth) * 100;
+  const height = ((naturalHeight * ratio) / naturalHeight) * 100;
+  const x = ((naturalWidth * (1 - width / 100)) / 2 / naturalWidth) * 100;
+  const y = ((naturalHeight * (1 - height / 100)) / 2 / naturalHeight) * 100;
+
+  return { x, y, width, height };
+};
+
 class ImgCrop extends Component {
-  static propTypes = {
-    modalTitle: PropTypes.string,
-    width: PropTypes.number,
-    height: PropTypes.number,
-    scale: PropTypes.number,
-    children: PropTypes.node,
-  };
-  static defaultProps = {
-    modalTitle: '编辑图片',
-    width: 100,
-    height: 100,
-    scale: 80,
-  };
   constructor(props) {
     super(props);
     const { width, height } = props;
     this.aspect = width / height;
-
-    this.initState = {
-      // Modal
-      modalVisible: false,
-      modalWidth: 520,
-      // ReactCrop
-      src: null,
-      crop: {},
-      pixelCrop: {},
-    };
-    this.state = this.initState;
+    this.state = defaultState;
   }
 
-  // Upload 组件
-  // --------------------------------------------------
-
+  /**
+   * Upload 组件
+   */
   // 渲染 Upload 组件
-  renderChildren = () => {
+  renderChildren() {
     const { children } = this.props;
     this.Upload = children;
 
@@ -59,9 +57,9 @@ class ImgCrop extends Component {
         beforeUpload: this.beforeUpload,
       },
     };
-  };
+  }
   // 格式化 beforeUpload 属性
-  beforeUpload = (file) => {
+  beforeUpload(file) {
     this.oldFile = file;
 
     // 读取添加的图片
@@ -78,51 +76,40 @@ class ImgCrop extends Component {
       this.resolve = resolve;
       this.reject = reject;
     });
-  };
+  }
 
-  // ReactCrop 组件
-  // --------------------------------------------------
-
+  /**
+   * ReactCrop 组件
+   */
   // 完成添加图片
-  onImageLoaded = (image) => {
+  onImageLoaded(image) {
     const { scale } = this.props;
     this.imageRef = image;
     const { naturalWidth, naturalHeight } = this.imageRef;
     const modalWidth = naturalWidth >= naturalHeight ? 640 + 24 * 2 : 320 + 24 * 2;
 
     let ratio = scale / 100;
-    let { x, y, width, height } = this.getCropValues(naturalWidth, naturalHeight, ratio);
+    let { x, y, width, height } = getCropValues(naturalWidth, naturalHeight, ratio, this.aspect);
     while (width > scale || height > scale) {
       ratio -= 0.02;
-      ({ x, y, width, height } = this.getCropValues(naturalWidth, naturalHeight, ratio));
+      ({ x, y, width, height } = getCropValues(naturalWidth, naturalHeight, ratio, this.aspect));
     }
 
     const crop = { aspect: this.aspect, x, y, width, height };
     const pixelCrop = getPixelCrop(this.imageRef, crop);
 
     this.setState({ modalWidth, crop, pixelCrop });
-  };
-  // 获取 crop 的值
-  getCropValues = (naturalWidth, naturalHeight, ratio) => {
-    // 注意，此处 width, height, x, y 均为百分比的值，如 "width: 80"，即为占比 "80%"
-    // @link: https://github.com/DominicTobias/react-image-crop#crop-required
-    const width = ((naturalHeight * ratio * this.aspect) / naturalWidth) * 100;
-    const height = ((naturalHeight * ratio) / naturalHeight) * 100;
-    const x = ((naturalWidth * (1 - width / 100)) / 2 / naturalWidth) * 100;
-    const y = ((naturalHeight * (1 - height / 100)) / 2 / naturalHeight) * 100;
-
-    return { x, y, width, height };
-  };
+  }
   // 响应裁切变化
-  onCropChange = (crop, pixelCrop) => {
+  onCropChange(crop, pixelCrop) {
     this.setState({ crop, pixelCrop });
-  };
+  }
 
-  // Modal 组件
-  // --------------------------------------------------
-
+  /**
+   * Modal 组件
+   */
   // 点击确定
-  onOk = async () => {
+  async onOk() {
     const { pixelCrop } = this.state;
     const { x, y, width, height } = pixelCrop;
 
@@ -140,7 +127,7 @@ class ImgCrop extends Component {
       const newFile = new File([blob], name, { type, lastModified: Date.now() });
       newFile.uid = uid;
 
-      this.setState(this.initState);
+      this.setState(defaultState);
 
       const { beforeUpload } = this.Upload.props;
       if (!beforeUpload) {
@@ -164,11 +151,11 @@ class ImgCrop extends Component {
         }
       }
     }, type);
-  };
+  }
   // 取消弹窗
-  onCancel = () => {
-    this.setState(this.initState);
-  };
+  onCancel() {
+    this.setState(defaultState);
+  }
 
   render() {
     const { modalTitle } = this.props;
@@ -198,5 +185,20 @@ class ImgCrop extends Component {
     );
   }
 }
+
+ImgCrop.propTypes = {
+  modalTitle: PropTypes.string,
+  width: PropTypes.number,
+  height: PropTypes.number,
+  scale: PropTypes.number,
+  children: PropTypes.node,
+};
+
+ImgCrop.defaultProps = {
+  modalTitle: '编辑图片',
+  width: 100,
+  height: 100,
+  scale: 80,
+};
 
 export default ImgCrop;
