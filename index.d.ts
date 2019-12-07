86974232491d6683e9cca3d529cef24f40e75a9f
nanxiaobei <nanxiaobei@gmail.com>
2019-12-08 01:09:37 +0800

    Update code style

--- index.d.ts
+++ index.d.ts
@@ -1,14 +1,17 @@
+// @ts-ignore
+import React, { Component } from 'react';
+
 declare module 'antd-img-crop' {
   export interface ImgCropProps {
-    width?:	number	// default: 100. If useRatio is true, it'll be ratio.
-    height?: number	// default: 100. If useRatio is true, it'll be ratio.
-    useRatio?: boolean	// default: false
-    resize?: boolean	// default: true	If crop can resize.
-    resizeAndDrag?:	boolean	// default: true	If crop can resize and drag.
-    modalTitle?: string	// default: "Edit image"	Modal title.
-    modalWidth?: number	// default: 520	Modal width in px.
-    beforeCrop?:	() => boolean  // Execute before crop, if return false, modal will not open (NO Promise support). Ant Design Upload beforeUpload prop will execute after crop, before upload.
+    width?: number;
+    height?: number;
+    useRatio?: boolean;
+    resize?: boolean;
+    resizeAndDrag?: boolean;
+    modalTitle?: string;
+    modalWidth?: number;
+    beforeCrop?: () => boolean;
   }
-  export class ImgCrop extends React.Component<ImgCropProps> {}
-  export default ImgCrop
+  export class ImgCrop extends Component<ImgCropProps> {}
+  export default ImgCrop;
 }
