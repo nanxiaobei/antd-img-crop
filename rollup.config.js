4f238cfd12759b4ae9f7fbe8563f6a81eddce943
nanxiaobei <nanxiaobei@gmail.com>
2020-02-02 20:48:59 +0800

    Add `@rollup/plugin-replace`

--- rollup.config.js
+++ rollup.config.js
@@ -1,3 +1,4 @@
+import replace from '@rollup/plugin-replace';
 import babel from 'rollup-plugin-babel';
 import sass from 'rollup-plugin-sass';
 import pkg from './package.json';
@@ -6,6 +7,7 @@ const input = 'src/index.jsx';
 const deps = [...Object.keys(pkg.peerDependencies), ...Object.keys(pkg.dependencies)];
 const external = (id) => deps.includes(id) || /antd\/|@babel\/runtime\//.test(id);
 const plugins = (isEsm) => [
+  !isEsm && replace({ '/es/': '/lib/' }),
   babel({
     plugins: [['@babel/plugin-transform-runtime', { useESModules: isEsm }]],
     runtimeHelpers: true,
