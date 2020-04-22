474e719694008cffb3512549291c79eaddca83df
nanxiaobei <nanxiaobei@gmail.com>
2020-04-23 04:02:22 +0800

    update plugins function param

--- rollup.config.js
+++ rollup.config.js
@@ -6,16 +6,16 @@ import pkg from './package.json';
 const input = 'src/index.jsx';
 const deps = [...Object.keys(pkg.peerDependencies), ...Object.keys(pkg.dependencies)];
 const external = (id) => deps.includes(id) || /antd\/|@babel\/runtime\//.test(id);
-const plugins = (isEsm) => [
-  !isEsm && replace({ '/es/': '/lib/' }),
+const plugins = (isCjs) => [
+  isCjs && replace({ '/es/': '/lib/' }),
   babel({
-    plugins: [['@babel/plugin-transform-runtime', { useESModules: isEsm }]],
+    plugins: [['@babel/plugin-transform-runtime', { useESModules: !isCjs }]],
     runtimeHelpers: true,
   }),
   sass({ insert: true }),
 ];
 
 export default [
-  { input, output: { file: pkg.main, format: 'cjs' }, external, plugins: plugins(false) },
-  { input, output: { file: pkg.module, format: 'es' }, external, plugins: plugins(true) },
+  { input, output: { file: pkg.main, format: 'cjs' }, external, plugins: plugins(true) },
+  { input, output: { file: pkg.module, format: 'es' }, external, plugins: plugins(false) },
 ];
