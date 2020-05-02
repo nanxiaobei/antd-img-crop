7b032981d06d76018d722b33aaa53f0613df3e7e
nanxiaobei <nanxiaobei@gmail.com>
2020-05-02 10:17:27 +0800

    refactor with react hooks and new crop lib

--- src/index.jsx
+++ src/index.jsx
@@ -1,348 +1,271 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
+import React, { useState, useCallback, useRef } from 'react';
+import t from 'prop-types';
+import Cropper from 'react-easy-crop';
 import LocaleReceiver from 'antd/es/locale-provider/LocaleReceiver';
 import Modal from 'antd/es/modal';
 import Slider from 'antd/es/slider';
-import ReactCrop from 'react-image-crop';
-import './index.scss';
+import './index.less';
 
+const pkg = 'antd-img-crop';
+const deprecateMap = {
+  width: 'aspect',
+  height: 'aspect',
+  contain: '',
+  resize: 'zoom',
+  resizeAndDrag: '',
+};
+const deprecate = (props) => {
+  Object.entries(deprecateMap).forEach(([key, val]) => {
+    if (props[key] === undefined) return;
+    let msg = `\`${key}\` is deprecated`;
+    if (val) msg += `, please use \`${val}\` instead`;
+    msg += `, see https://github.com/nanxiaobei/${pkg}`;
+    console.error(msg);
+  });
+};
+
+const MEDIA_CLASS = `${pkg}-media`;
 const MODAL_TITLE = 'Edit image';
-const ROTATE_LABEL = 'Rotate';
 
-class ImgCrop extends Component {
-  constructor(props) {
-    super(props);
+const EasyCrop = (props) => {
+  const {
+    src,
+    aspect,
+    shape,
+    hasZoom,
+    zoomVal,
+    rotateVal,
+    setZoomVal,
+    setRotateVal,
+    onComplete,
+  } = props;
+  const [crop, setCrop] = useState({ x: 0, y: 0 });
+  const onCropComplete = useCallback(
+    (croppedArea, croppedAreaPixels) => {
+      onComplete(croppedAreaPixels);
+    },
+    [onComplete],
+  );
 
-    this.hasRotate = props.rotate === true;
-    this.state = {
-      modalVisible: false,
-      src: null,
-      crop: {},
-      degree: 0,
-    };
-  }
+  return (
+    <Cropper
+      image={src}
+      aspect={aspect}
+      cropShape={shape}
+      zoomWithScroll={hasZoom}
+      crop={crop}
+      zoom={zoomVal}
+      rotation={rotateVal}
+      onCropChange={setCrop}
+      onZoomChange={setZoomVal}
+      onRotationChange={setRotateVal}
+      onCropComplete={onCropComplete}
+      classes={{ containerClassName: `${pkg}-container`, mediaClassName: MEDIA_CLASS }}
+      showGrid={false}
+    />
+  );
+};
 
-  /**
-   * Upload 组件
-   */
-  // 渲染 Upload 组件
-  renderUpload = () => {
-    const { children } = this.props;
-    const uploadComponent = Array.isArray(children) ? children[0] : children;
+EasyCrop.propTypes = {
+  src: t.string,
+  aspect: t.number,
+  shape: t.string,
+  hasZoom: t.bool,
+  zoomVal: t.number,
+  rotateVal: t.number,
+  setZoomVal: t.func,
+  setRotateVal: t.func,
+  onComplete: t.func,
+};
 
-    if (this.newUploadProps === undefined) {
-      const { accept = 'image/*', beforeUpload = () => true } = uploadComponent.props;
-      this.realBeforeUpload = beforeUpload;
+const ImgCrop = (props) => {
+  if (process.env.NODE_ENV !== 'production') deprecate(props);
 
-      this.newUploadProps = { accept, beforeUpload: this.beforeUpload };
-    }
+  const { aspect, shape, zoom, rotate, beforeCrop, modalTitle, modalWidth, children } = props;
+  const hasZoom = zoom === true;
+  const hasRotate = rotate === true;
 
-    return {
-      ...uploadComponent,
-      props: {
-        ...uploadComponent.props,
-        ...this.newUploadProps,
-      },
-    };
-  };
-  // 格式化 beforeUpload 属性
-  beforeUpload = (file, fileList) => {
-    return new Promise((resolve, reject) => {
-      this.resolve = resolve;
-      this.reject = reject;
+  const [src, setSrc] = useState('');
+  const [zoomVal, setZoomVal] = useState(1);
+  const [rotateVal, setRotateVal] = useState(0);
 
-      // 裁剪前校验图片
-      const { beforeCrop } = this.props;
-      if (beforeCrop && !beforeCrop(file, fileList)) {
-        this.reject();
-        return;
-      }
-
-      this.oldFile = file;
-
-      // 读取添加的图片
-      const reader = new FileReader();
-      reader.addEventListener('load', () => {
-        this.setState({ modalVisible: true, src: reader.result });
-      });
-      reader.readAsDataURL(file); // then -> `onImageLoaded`
-    });
-  };
-
-  /**
-   * ReactCrop 组件
-   */
-  // 完成添加图片
-  onImageLoaded = (image) => {
-    if (this.image !== undefined) return;
-
-    this.image = image;
-
-    // real** 实际大小，show** 显示大小
-    const { naturalWidth: realImgWidth, naturalHeight: realImgHeight } = image;
-    const { width: realCropWidth, height: realCropHeight, modalWidth } = this.props;
-
-    const cropRate = realCropWidth / realCropHeight;
-    const modalBodyWidth = modalWidth - 24 * 2;
-
-    let scale = 1;
-    let showImgWidth;
-    let showImgHeight;
-
-    let showCropWidth;
-    let showCropHeight;
-
-    let showCropX;
-    let showCropY;
-
-    let contain;
-    if (realCropWidth > realImgWidth || realCropHeight > realImgHeight) {
-      contain = true;
-    } else {
-      contain = this.props.contain;
-    }
-
-    // 设置数值大小
-    const setNumberData = () => {
-      showCropWidth = realCropWidth / scale;
-      showCropHeight = realCropHeight / scale;
-
-      showCropX = (showImgWidth - showCropWidth) / 2;
-      showCropY = (showImgHeight - showCropHeight) / 2;
-    };
-
-    // 设置填充容器
-    const setContainData = () => {
-      const imgRate = realImgWidth / realImgHeight;
-
-      if (cropRate > imgRate) {
-        // 裁剪框宽度大于图片
-        showCropWidth = showImgWidth;
-        showCropHeight = showCropWidth / cropRate;
-
-        showCropX = 0;
-        showCropY = (showImgHeight - showCropHeight) / 2;
-      } else {
-        // 裁剪框宽度小于图片
-        showCropHeight = showImgHeight;
-        showCropWidth = showCropHeight * cropRate;
-
-        showCropX = (showImgWidth - showCropWidth) / 2;
-        showCropY = 0;
-      }
-    };
-
-    // 设置数值大小 or 填充容器
-    const setCropData = () => {
-      contain === true ? setContainData() : setNumberData();
-    };
-
-    // 设置裁切相关值
-    if (realImgWidth > modalBodyWidth) {
-      // 图片宽度大于 Model
-      showImgWidth = modalBodyWidth;
-      scale = realImgWidth / showImgWidth;
-      showImgHeight = realImgHeight / scale;
-
-      this.scale = scale;
-
-      setCropData();
-    } else {
-      // 图片宽度小于 Model
-      showImgWidth = realImgWidth;
-      showImgHeight = realImgHeight;
-
-      setCropData();
-    }
-
-    this.setState({
-      crop: {
-        unit: 'px',
-        aspect: cropRate,
-        width: showCropWidth,
-        height: showCropHeight,
-        x: showCropX,
-        y: showCropY,
-      },
-    });
-
-    return false;
-  };
-  // 响应裁切变化
-  onCropChange = (crop) => {
-    this.setState({ crop });
-  };
+  const dataRef = useRef({});
+  const data = dataRef.current;
 
   /**
-   * Modal 组件
+   * Upload
    */
-  // 点击确定
-  onOk = async () => {
-    const { crop } = this.state;
-    let { width: sWidth, height: sHeight } = crop;
+  const renderUpload = useCallback(() => {
+    const upload = Array.isArray(children) ? children[0] : children;
+    if (!data.uploadProps) {
+      const { accept, beforeUpload } = upload.props;
+      data.beforeUpload = beforeUpload;
+      data.uploadProps = {
+        accept: accept || 'image/*',
+        beforeUpload: (file, fileList) =>
+          new Promise((resolve, reject) => {
+            if (beforeCrop && !beforeCrop(file, fileList)) {
+              reject();
+              return;
+            }
 
-    if (!sWidth || !sHeight) {
-      this.onClose();
-      return;
+            data.file = file;
+            data.resolve = resolve;
+            data.reject = reject;
+
+            const reader = new FileReader();
+            reader.addEventListener('load', () => {
+              setSrc(reader.result);
+            });
+            reader.readAsDataURL(file);
+          }),
+      };
     }
+    return { ...upload, props: { ...upload.props, ...data.uploadProps } };
+  }, [data, children, beforeCrop]);
 
-    // 重绘图片如果其已旋转
-    let img = this.image;
-    const { degree } = this.state;
+  /**
+   * EasyCrop
+   */
+  const onComplete = useCallback(
+    (croppedAreaPixels) => {
+      data.croppedAreaPixels = croppedAreaPixels;
+    },
+    [data],
+  );
 
-    if (this.hasRotate && degree > 0 && degree < 360) {
-      const newCanvas = document.createElement('canvas');
-      const { naturalWidth, naturalHeight } = img;
-      newCanvas.width = naturalWidth;
-      newCanvas.height = naturalHeight;
-      const newCtx = newCanvas.getContext('2d');
+  /**
+   * Modal
+   */
+  const onClose = useCallback(() => {
+    setSrc('');
+    setZoomVal(1);
+    setRotateVal(0);
+    dataRef.current = {};
+  }, []);
 
-      const halfWidth = naturalWidth / 2;
-      const halfHeight = naturalHeight / 2;
+  const onOk = useCallback(async () => {
+    onClose();
 
-      newCtx.translate(halfWidth, halfHeight);
-      newCtx.rotate((degree * Math.PI) / 180);
-      newCtx.translate(-halfWidth, -halfHeight);
-
-      newCtx.drawImage(img, 0, 0);
-      img = newCanvas;
-    }
-
-    // 获取裁切后的图片
-    let { x: sx, y: sy } = crop;
-    const scale = this.scale;
-    if (scale !== undefined) {
-      sx = sx * scale;
-      sy = sy * scale;
-      sWidth = sWidth * scale;
-      sHeight = sHeight * scale;
-    }
-    const { width: dWidth, height: dHeight } = this.props;
+    const img = document.querySelector(`.${MEDIA_CLASS}`);
+    const { naturalWidth, naturalHeight } = img;
     const canvas = document.createElement('canvas');
-    canvas.width = dWidth;
-    canvas.height = dHeight;
     const ctx = canvas.getContext('2d');
-    ctx.drawImage(img, sx, sy, sWidth, sHeight, 0, 0, dWidth, dHeight);
 
-    const { name, type, uid } = this.oldFile;
+    // create a max canvas to cover the source image after rotated
+    const maxLen = Math.sqrt(Math.pow(naturalWidth, 2) + Math.pow(naturalHeight, 2));
+    canvas.width = maxLen;
+    canvas.height = maxLen;
+
+    // rotate the image
+    if (hasRotate && rotateVal > 0 && rotateVal < 360) {
+      const halfMax = maxLen / 2;
+      ctx.translate(halfMax, halfMax);
+      ctx.rotate((rotateVal * Math.PI) / 180);
+      ctx.translate(-halfMax, -halfMax);
+    }
+
+    // draw the source image in the center of the max canvas
+    const left = (maxLen - naturalWidth) / 2;
+    const top = (maxLen - naturalHeight) / 2;
+    ctx.drawImage(img, left, top);
+
+    // shrink the max canvas to the crop area size, then align two center points
+    const maxImgData = ctx.getImageData(0, 0, maxLen, maxLen);
+    const { width, height, x, y } = data.croppedAreaPixels;
+    canvas.width = width;
+    canvas.height = height;
+    ctx.putImageData(maxImgData, -left - x, -top - y);
+
+    // get the new image
+    const { beforeUpload = () => true, file, resolve, reject } = data;
     canvas.toBlob(async (blob) => {
-      const croppedFile = new File([blob], name, { type, lastModified: Date.now() }); // 生成新图片
-      croppedFile.uid = uid;
+      blob.lastModifiedDate = Date.now();
+      blob.name = file.name;
+      blob.uid = file.uid;
 
-      this.onClose(); // 关闭弹窗
-      const response = this.realBeforeUpload(croppedFile, [croppedFile]); // 调用 beforeUpload
-
-      if (response === false) {
-        this.reject();
-        return;
+      const res = beforeUpload(blob, [blob]);
+      if (res === false) return reject();
+      if (res === true) return resolve(blob);
+      if (typeof res.then === 'function') {
+        try {
+          const newFile = await res;
+          const fileType = Object.prototype.toString.call(newFile);
+          resolve(fileType === '[object File]' || fileType === '[object Blob]' ? newFile : blob);
+        } catch (err) {
+          reject(err);
+        }
       }
-
-      if (typeof response.then !== 'function') {
-        this.resolve(croppedFile);
-        return;
-      }
-
-      try {
-        const croppedProcessedFile = await response;
-        const fileType = Object.prototype.toString.call(croppedProcessedFile);
-        const useProcessedFile = fileType === '[object File]' || fileType === '[object Blob]';
-
-        this.resolve(useProcessedFile ? croppedProcessedFile : croppedFile);
-      } catch (err) {
-        this.reject(err);
-      }
-    }, type);
-  };
-  // 关闭弹窗
-  onClose = () => {
-    this.oldFile = undefined;
-    this.image = undefined;
-    this.scale = undefined;
-    this.setState({
-      modalVisible: false,
-      crop: {},
-      degree: 0,
     });
-  };
-  // 旋转图片
-  onRotate = (degree) => {
-    this.setState({ degree });
-  };
+  }, [data, onClose, hasRotate, rotateVal]);
 
-  render() {
-    const { modalTitle, modalWidth, resize, resizeAndDrag, rotateLabel } = this.props;
-    const { modalVisible, src, crop, degree } = this.state;
-
-    return (
-      <LocaleReceiver>
-        {(locale, localeCode) => (
-          <>
-            {this.renderUpload()}
+  return (
+    <LocaleReceiver>
+      {(locale, localeCode) => (
+        <>
+          {renderUpload()}
+          {src && (
             <Modal
-              visible={modalVisible}
-              width={modalWidth}
-              onOk={this.onOk}
-              onCancel={this.onClose}
-              wrapClassName="antd-img-crop-modal"
+              visible={true}
+              wrapClassName={`${pkg}-modal`}
               title={localeCode === 'zh-cn' && modalTitle === MODAL_TITLE ? '编辑图片' : modalTitle}
+              width={modalWidth}
+              onOk={onOk}
+              onCancel={onClose}
               maskClosable={false}
               destroyOnClose
             >
-              {src && (
-                <>
-                  <ReactCrop
-                    src={src}
-                    crop={crop}
-                    locked={resize === false}
-                    disabled={resizeAndDrag === false}
-                    onImageLoaded={this.onImageLoaded}
-                    onChange={this.onCropChange}
-                    keepSelection
-                    imageStyle={{ transform: `rotate(${degree}deg)` }}
-                  />
-                  {this.hasRotate && (
-                    <div className="action">
-                      <span className="label">{rotateLabel}</span>
-                      <Slider min={0} max={360} value={degree} onChange={this.onRotate} />
-                    </div>
-                  )}
-                </>
+              <EasyCrop
+                src={src}
+                aspect={aspect}
+                shape={shape}
+                hasZoom={hasZoom}
+                zoomVal={zoomVal}
+                rotateVal={rotateVal}
+                setZoomVal={setZoomVal}
+                setRotateVal={setRotateVal}
+                onComplete={onComplete}
+              />
+              {hasZoom && (
+                <div className={`${pkg}-action zoom`}>
+                  <i>－</i>
+                  <Slider min={1} max={3} step={0.1} value={zoomVal} onChange={setZoomVal} />
+                  <i>＋</i>
+                </div>
+              )}
+              {hasRotate && (
+                <div className={`${pkg}-action rotate`}>
+                  <i>↻</i>
+                  <Slider min={0} max={360} value={rotateVal} onChange={setRotateVal} />
+                  <i>↻</i>
+                </div>
               )}
             </Modal>
-          </>
-        )}
-      </LocaleReceiver>
-    );
-  }
-}
+          )}
+        </>
+      )}
+    </LocaleReceiver>
+  );
+};
 
 ImgCrop.propTypes = {
-  width: PropTypes.number,
-  height: PropTypes.number,
-  contain: PropTypes.bool,
-  resize: PropTypes.bool,
-  resizeAndDrag: PropTypes.bool,
-
-  modalTitle: PropTypes.string,
-  modalWidth: PropTypes.number,
-  beforeCrop: PropTypes.func,
-  rotate: PropTypes.bool,
-  rotateLabel: PropTypes.string,
-
-  children: PropTypes.node,
+  aspect: t.number,
+  shape: t.oneOf(['rect', 'round']),
+  zoom: t.bool,
+  rotate: t.bool,
+  beforeCrop: t.func,
+  modalTitle: t.string,
+  modalWidth: t.oneOfType([t.number, t.string]),
+  children: t.node,
 };
 
 ImgCrop.defaultProps = {
-  width: 100,
-  height: 100,
-  contain: false,
-  resize: true,
-  resizeAndDrag: true,
-
+  aspect: 1,
+  shape: 'rect',
+  zoom: true,
+  rotate: false,
   modalTitle: MODAL_TITLE,
   modalWidth: 520,
-  rotate: false,
-  rotateLabel: ROTATE_LABEL,
 };
 
 export default ImgCrop;
