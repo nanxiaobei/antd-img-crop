129c6ea6de1cc4c1060ad6fde3a33bf3bafdfc61
nanxiaobei <nanxiaobei@gmail.com>
2020-06-01 18:34:54 +0800

    add missing typings

--- index.d.ts
+++ index.d.ts
@@ -3,11 +3,14 @@ import * as React from 'react';
 export interface ImgCropProps {
   aspect?: number;
   shape?: 'rect' | 'round';
+  grid?: boolean;
   zoom?: boolean;
   rotate?: boolean;
   beforeCrop?: (file: File, fileList: File[]) => boolean;
   modalTitle?: string;
   modalWidth?: number | string;
+  modalOk?: string;
+  modalCancel?: string;
 }
 declare const ImgCrop: React.FC<ImgCropProps>;
 export default ImgCrop;
