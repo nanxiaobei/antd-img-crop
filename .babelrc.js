1a812fbf2ec25a0bbf12dbc955006799785a580c
nanxiaobei <nanxiaobei@gmail.com>
2020-06-10 06:57:52 +0800

    merge .babelrc.js to rollup.config.js

--- .babelrc.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  presets: [
-    ['@babel/preset-env', { targets: '> 0.25%, not dead', modules: false, loose: true }],
-    ['@babel/preset-react', { useBuiltIns: true }],
-  ],
-};
