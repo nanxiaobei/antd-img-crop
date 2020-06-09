f79aef5565b212580b3238b6eb965742b48fd497
nanxiaobei <nanxiaobei@gmail.com>
2020-06-10 06:58:11 +0800

    update rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -1,21 +1,25 @@
-import replace from '@rollup/plugin-replace';
 import babel from 'rollup-plugin-babel';
 import less from 'rollup-plugin-less';
 import pkg from './package.json';
 
 const input = 'src/index.jsx';
-const deps = [...Object.keys(pkg.peerDependencies), ...Object.keys(pkg.dependencies)];
-const external = (id) => deps.includes(id) || /antd\/|@babel\/runtime\//.test(id);
-const plugins = (isCjs) => [
-  isCjs && replace({ '/es/': '/lib/' }),
+const external = (id) => !/index.less$/.test(id);
+const plugins = (isEsm) => [
   babel({
-    plugins: [['@babel/plugin-transform-runtime', { useESModules: !isCjs }]],
+    presets: [
+      ['@babel/preset-env', { targets: '> 0.25%, not dead', modules: false, loose: true }],
+      ['@babel/preset-react', { useBuiltIns: true }],
+    ],
+    plugins: [
+      ['import', { libraryName: 'antd', libraryDirectory: isEsm ? 'es' : 'lib', style: true }],
+      ['@babel/plugin-transform-runtime', { useESModules: isEsm }],
+    ],
     runtimeHelpers: true,
   }),
   less({ insert: true, output: false }),
 ];
 
 export default [
-  { input, output: { file: pkg.main, format: 'cjs' }, external, plugins: plugins(true) },
-  { input, output: { file: pkg.module, format: 'es' }, external, plugins: plugins(false) },
+  { input, output: { file: pkg.main, format: 'cjs' }, external, plugins: plugins(false) },
+  { input, output: { file: pkg.module, format: 'es' }, external, plugins: plugins(true) },
 ];
