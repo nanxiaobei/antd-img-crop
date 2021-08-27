144e89111a27fd2830d41b17e191e803c5d63ef6
nanxiaobei <nanxiaobei@gmail.com>
2021-08-28 07:52:32 +0800

    add new props

--- index.d.ts
+++ index.d.ts
@@ -17,8 +17,11 @@ export interface ImgCropProps {
   modalWidth?: number | string;
   modalOk?: string;
   modalCancel?: string;
+  onModalOk?: (file: File) => void;
+  onModalCancel?: () => void;
 
   beforeCrop?: (file: File, fileList: File[]) => boolean;
+  onUploadFail?: (err: Error) => void;
   cropperProps?: Partial<CropperProps>;
 }
 declare const ImgCrop: React.FC<ImgCropProps>;
