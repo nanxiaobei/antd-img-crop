d09ff2d6d30f2a98adf9897c4680e8d2e55920e8
nanxiaobei <nanxiaobei@gmail.com>
2021-12-18 03:44:12 +0800

    update rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -1,3 +1,4 @@
+import replace from '@rollup/plugin-replace';
 import babel from 'rollup-plugin-babel';
 import less from 'rollup-plugin-less';
 import pkg from './package.json';
@@ -5,13 +6,14 @@ import pkg from './package.json';
 const input = 'src/index.jsx';
 const exports = 'auto';
 const external = (id) => !/index.less$/.test(id);
-const plugins = (useESModules) => [
+const plugins = (isESM) => [
+  !isESM && replace({ preventAssignment: true, '/es/': '/lib/' }),
   babel({
     presets: [
       ['@babel/preset-env', { targets: '> 0.25%, not dead', modules: false, loose: true }],
       ['@babel/preset-react', { useBuiltIns: true }],
     ],
-    plugins: [['@babel/plugin-transform-runtime', { useESModules }]],
+    plugins: [['@babel/plugin-transform-runtime', { useESModules: isESM }]],
     runtimeHelpers: true,
   }),
   less({ insert: true, output: false }),
