4aa52085f30acb73949abed11190fa1780bfec08
nanxiaobei <nanxiaobei@gmail.com>
2022-03-17 08:21:02 +0800

    use typescript

--- rollup.config.js
+++ rollup.config.js
@@ -1,21 +1,15 @@
+import typescript from '@rollup/plugin-typescript';
 import replace from '@rollup/plugin-replace';
-import babel from 'rollup-plugin-babel';
 import less from 'rollup-plugin-less';
 import pkg from './package.json';
 
-const input = 'src/index.jsx';
+const input = 'src/img-crop.tsx';
 const exports = 'auto';
-const external = (id) => !/index.less$/.test(id);
+const deps = [...Object.keys(pkg.dependencies), ...Object.keys(pkg.peerDependencies)];
+const external = (id) => deps.includes(id) || id.startsWith('antd');
 const plugins = (isESM) => [
+  typescript(),
   !isESM && replace({ preventAssignment: true, '/es/': '/lib/' }),
-  babel({
-    presets: [
-      ['@babel/preset-env', { targets: '> 0.25%, not dead', modules: false, loose: true }],
-      ['@babel/preset-react', { useBuiltIns: true }],
-    ],
-    plugins: [['@babel/plugin-transform-runtime', { useESModules: isESM }]],
-    runtimeHelpers: true,
-  }),
   less({ insert: true, output: false }),
 ];
 
