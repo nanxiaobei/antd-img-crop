5e85c3faf97dd0e4d22b5d5a80201d7e259aa4f5
nanxiaobei <nanxiaobei@gmail.com>
2023-01-27 15:01:28 +0800

    remove @rollup/plugin-replace

--- rollup.config.ts
+++ rollup.config.ts
@@ -1,6 +1,5 @@
 import dts from 'rollup-plugin-dts';
 import less from 'rollup-plugin-less';
-import replace from '@rollup/plugin-replace';
 import typescript from '@rollup/plugin-typescript';
 import { readFileSync } from 'fs';
 
@@ -13,21 +12,15 @@ const dtsOutput = { file: pkg.types, format: 'es' };
 
 const tsPlugin = typescript();
 const lessPlugin = less({ insert: true, output: false });
-const replacePlugin = replace({
-  preventAssignment: true,
-  'LocaleReceiver, null': 'LocaleReceiver.default, null',
-});
-const cjsPlugins = [tsPlugin, lessPlugin, replacePlugin];
-const esmPlugins = [tsPlugin, lessPlugin];
 
 const external = [
   ...Object.keys({ ...pkg.dependencies, ...pkg.peerDependencies }),
-  'react/jsx-runtime',
-  /^antd/,
+  /^react($|\/)/,
+  /^antd($|\/)/,
 ];
 
 export default [
-  { input, output: cjsOutput, plugins: cjsPlugins, external },
-  { input, output: esmOutput, plugins: esmPlugins, external },
+  { input, output: cjsOutput, plugins: [tsPlugin, lessPlugin], external },
+  { input, output: esmOutput, plugins: [tsPlugin, lessPlugin], external },
   { input, output: dtsOutput, plugins: [dts()], external: [/\.less$/] },
 ];
