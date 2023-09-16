427f945d572e6d3c575d934e9123f189004d0e2d
nanxiaobei <nanxiaobei@gmail.com>
2023-09-17 05:44:02 +0800

    update prettier config

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,25 +1,5 @@
 module.exports = {
   singleQuote: true,
-
-  plugins: [
-    require.resolve('@trivago/prettier-plugin-sort-imports'),
-    require.resolve('prettier-plugin-tailwindcss'),
-  ],
-  pluginSearchDirs: false,
-
-  importOrder: [
-    '^react',
-    '^antd',
-    '^rollup',
-    '^@rollup',
-    '^vite',
-    '^@vite',
-    '<THIRD_PARTY_MODULES>',
-    'constants',
-    'types',
-    '^[./]',
-  ],
-  importOrderSortSpecifiers: true,
-  importOrderGroupNamespaceSpecifiers: true,
-  importOrderCaseInsensitive: true,
+  plugins: ['prettier-plugin-organize-imports', 'prettier-plugin-tailwindcss'],
+  organizeImportsSkipDestructiveCodeActions: true,
 };
